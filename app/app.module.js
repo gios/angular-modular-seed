7d54b0032a4eeb478467d62c2b47cb23eb4c87d0
gios <pblazhchuk@gmail.com>
2015-01-16 16:07:20 +0200

    test1

--- app/app.module.js
+++ app/app.module.js
@@ -2,5 +2,5 @@ var angular = require("angular");
 angular
 	.module('test', [])
 	.controller('gg', ['$scope', function($scope){
-		$scope.text = "Hello World Everybody!";
+		$scope.text = "Hello World!";
 	}]);
